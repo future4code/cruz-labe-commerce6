91f04124a384daa03d6ff19d57b8f351385db69d
GuilhermeSoutello <guisoutello@gmail.com>
2021-03-18 12:37:55 -0300

    Implementação do conteúdo estático.
    Co-authored-by: Fagner Alexandre Zulin <fagnerzulin11@gmail.com>
    Co-authored-by: MarioCaiafaFilho <MarioCaiafaFilho@users.noreply.github.com>

--- src/components/ListaDeProdutos/CardProduto/CardProduto.js
+++ src/components/ListaDeProdutos/CardProduto/CardProduto.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+    width: 15vw;
+    border: 1px solid black;
+    height: 300px;  
+
+    img{
+        max-width:100%
+    }
+
+`
 
 export default class CardProduto extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <CardContainer>
+                <img src={"https://picsum.photos/200/200?a=1"} alt="Imagem Produto"/>
+                <p>Foguete da Missão Apollo 11</p>
+                <p>R$ 10000,00</p>
+            </CardContainer>
         )
     }
 }
