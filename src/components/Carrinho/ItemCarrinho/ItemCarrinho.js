16ebb4716579bfca22aef54000e0c68f0bee44cf
GuilhermeSoutello <guisoutello@gmail.com>
2021-03-19 15:42:32 -0300

    Projeto finalizado.
    Co-authored-by: Fagner Alexandre Zulin <fagnerzulin11@gmail.com>

--- src/components/Carrinho/ItemCarrinho/ItemCarrinho.js
+++ src/components/Carrinho/ItemCarrinho/ItemCarrinho.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const LinhaProduto = styled.div`
+    display:flex;
+    justify-content: space-evenly;
+`
 
 export default class ItemCarrinho extends Component {
     render() {
+        const {id, nome, quantidade, preco} = this.props.info
+        
         return (
-            <div>
-                
-            </div>
+            <LinhaProduto>
+                <span>{quantidade}x</span>
+                <span>{nome}</span>
+                <button onClick={() => this.props.onClickRemoverProduto(id, preco)}>Remover</button>
+            </LinhaProduto>
         )
     }
 }
--- src/components/ListaDeProdutos/CardProduto/CardProduto.js
+++ src/components/ListaDeProdutos/CardProduto/CardProduto.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { transformaEmMoeda } from '../../../utils/transformaEmMoeda'
 
 const CardContainer = styled.div`
     width: 15vw;
     border: 1px solid black;
-    height: 300px;  
+    height: 350px;  
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     img{
         max-width:100%
@@ -12,13 +17,27 @@ const CardContainer = styled.div`
 
 `
 
+    
+
 export default class CardProduto extends Component {
+    adicionaAoCarrinho = () =>{
+        const item ={
+            id: this.props.id,
+            nome: this.props.nome,
+            preco:this.props.valor,
+            valor: this.props.valor,
+            quantidade: 1
+        }
+
+        this.props.onClickAdicionarProduto(item)
+    }
     render() {
         return (
             <CardContainer>
-                <img src={"https://picsum.photos/200/200?a=1"} alt="Imagem Produto"/>
-                <p>Foguete da Missão Apollo 11</p>
-                <p>R$ 10000,00</p>
+                <img src={this.props.imagem} alt="Imagem Produto"/>
+                <p>{this.props.nome}</p>
+                <p>{transformaEmMoeda(this.props.valor)}</p>
+                <button onClick={this.adicionaAoCarrinho}>Adicionar ao Carrinho</button>
             </CardContainer>
         )
     }
